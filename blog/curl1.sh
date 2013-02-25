ab7c3762d212fd43d89739071b13a1ba1cb2ec8c
Albert Asemwota <semo@berkeley.edu>
2013-02-24 23:46:24 -0800

    Fixed Welcome page

--- blog/curl1.sh
+++ blog/curl1.sh
@@ -1,1 +1,1 @@
-curl -v -H "Content-type: application/json" -X POST -d '{"user":"user1","password": "PASS1"}' -i "http://localhost:3000/users/login"
+curl -v -H "Content-type: application/json" -X POST -d '{"user":"user1","password": "PASS1"}' -i "http://fierce-oasis-3063.herokuapp.com/users/login"
